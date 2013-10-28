1378c9a6a920f06d5a9b6976472284ba06d7033b
LineMetrics <tec-team@linemetrics.com>
2013-10-28 11:32:13 +0100

    Create log.erl

---
+++ src/log.erl
@@ -0,0 +1,28 @@
+%% LineMetrics 2013
+-module(log).
+
+-export([put/3, put/2]).
+
+%% 0: nothing
+%% 1: important things
+%% 2: even more things
+%% 3: most things
+%% 4: all
+
+-define(LEVEL,4).
+
+put(NeededLevel, Message, Parameter) ->
+    if
+       NeededLevel =< ?LEVEL ->
+	    %%error_logger:info_report([{message, Message}, {parameter, Parameter}]),
+	    io:format(("Log: " ++ Message++"~n"), Parameter);
+       true -> {}
+    end.
+
+put(NeededLevel, Message) ->
+    if
+       NeededLevel =< ?LEVEL ->
+	    io:format(("Log: " ++ Message++"~n"));
+       true -> {}
+    end.
+			   
